8d0026597b1fab33cab0f6969943aca39e95149a
Danie1zika <danielbrandao03420@gmail.com>
2024-04-24 08:41:51 -0400

    questao21

---
+++ questao21.js
@@ -0,0 +1,6 @@
+/**Questão 21 - Existe alguma diferença de desempenho entre map() e 
+forEach()? */
+
+
+//map: retorna um arrayt completamente cono e não altera o array original ao qual ele foi chamado.
+//forEach: não retorna a nada "undefined". Ele apenas chama uma função fornecida em cada elemento do seu array.
