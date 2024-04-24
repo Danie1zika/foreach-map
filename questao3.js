59b5f81df0c909bac689b4f0d7dc08d103dfa3b3
Danie1zika <danielbrandao03420@gmail.com>
2024-04-24 08:40:19 -0400

    questao3

---
+++ questao3.js
@@ -0,0 +1,8 @@
+/**Questão 03 – Crie um algoritmo usando o método forEach() para encontrar o 
+maior número em um array de números? */
+
+
+const meuArray = [1, 2, 3, 4, 5];
+meuArray.forEach(function(elemento) {
+    console.log(elemento);
+});
